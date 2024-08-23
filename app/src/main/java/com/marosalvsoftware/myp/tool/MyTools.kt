7af7e622413e349d5c6e46ddc1ce3cff5af0b6c0
Salvatore <marosalv@gmail.com>
2024-08-23 20:16:44 +0200

    Completed MyTools myMergeSort() and myHeapSort tested

--- app/src/main/java/com/marosalvsoftware/myp/tool/MyTools.kt
+++ app/src/main/java/com/marosalvsoftware/myp/tool/MyTools.kt
@@ -10,17 +10,14 @@ fun IntArray.myHeapSort(): IntArray {
         heapify(arr, arr.size, i)
 
     for (i in size - 1 downTo 0) {
-        counter++
         arr[0] = arr[i].also { arr[i] = arr[0] }
         heapify(arr, i, 0)
     }
     return arr
 }
 
-var counter = 0
 private fun heapify(arr: IntArray, size: Int, parent: Int) {
 
-    println(arr.contentToString())
     var largest = parent
     val l = 2 * parent + 1
     val r = 2 * parent + 2
@@ -31,52 +28,57 @@ private fun heapify(arr: IntArray, size: Int, parent: Int) {
     if (largest != parent) {
         arr[parent] = arr[largest].also { arr[largest] = arr[parent] }
         heapify(arr, size, largest)
-        counter++
-        println("Changed $counter times")
     }
 }
 
 
-fun IntArray.mergeSort() :IntArray{
-
-return 
+fun IntArray.myMergeSort(): IntArray {
+    return splitAndMerge(this)
 }
 
-private fun splitAndMerge(arr: IntArray) : IntArray{
+private fun splitAndMerge(arr: IntArray): IntArray {
 
-val mid = arr.size -1 /2
-var subLeft = arr.mySplitter(0, mid)
-var subRight = are.mySplitter(mid+1,are.size - 1)
+    val mid = (arr.size - 1) / 2
+    var subLeft = arr.mySplitter(start = 0,end = mid)
+    var subRight = arr.mySplitter(start = mid + 1,end =  arr.size - 1)
 
-if(subLeft.size > 1)
-subLeft = splitAndMerge(subLeft)
+    if (subLeft.size > 1)
+        subLeft = splitAndMerge(subLeft)
 
-if(subRight.size > 1)
-subRight = splitAndMerge(subRight)
+    if (subRight.size > 1)
+        subRight = splitAndMerge(subRight)
 
-return mergeArrange(subLeft, subRight)
+    return mergeArrange(subLeft, subRight)
 
 }
 
-private fun mergeArrange(left : IntArray, right :IntArray) : IntArray{
-val result = IntArray(left.size + right.size)
-var r = 0
-var l = 0
+private fun mergeArrange(left: IntArray, right: IntArray): IntArray {
+    val result = IntArray(left.size + right.size)
+    var r = 0
+    var l = 0
 
-for(i in result.indices){
-if(l < left.size && r < right.size){
-if(left[l] < right[r]){
-result[i] = left[l++]
-}else{
-result[i] = right[r++]
-}
-}else if(l < left.size){
-result[i] = left[l++]
-}else{
-result[i] = right[r++]
-}
+    for (i in result.indices) {
+        if (l < left.size && r < right.size) {
+            if (left[l] < right[r]) {
+                result[i] = left[l++]
+            } else {
+                result[i] = right[r++]
+            }
+        } else if (l < left.size) {
+            result[i] = left[l++]
+        } else {
+            result[i] = right[r++]
+        }
+    }
+
+    return result
+
 }
 
-return result
-
+private fun IntArray.mySplitter(start: Int, end: Int): IntArray {
+    val result = IntArray(end - start + 1)
+    for (i in result.indices) {
+        result[i] = this[start + i]
+    }
+    return result
 }
